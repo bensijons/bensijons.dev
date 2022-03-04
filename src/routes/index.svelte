b91e54c4e1018de0c0645da2e9351c617c59659a
Benedikt Jónsson <bensijons@gmail.com>
2022-03-04 14:52:56 +0000

    stylish

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,19 @@
-<h1>Hi</h1>
-<p>My name is Benedikt but you can call me Bensi.</p>
-<p>I work <a href="https://gangverk.is">@Gangverk</a></p>
-<p>For those wondering, the answer is yes, I created this website all by myself</p>
+<section>
+	<h1>Hi</h1>
+	<p>My name is Benedikt but you can call me Bensi</p>
+	<p>I work <a href="https://gangverk.is">@Gangverk</a></p>
+	<p>My handle is @bensijons on most places</p>
+	<p>For those wondering, the answer is yes, I created this website all by myself</p>
+</section>
+
+<style>
+	section {
+		display: grid;
+		justify-content: center;
+		align-items: center;
+	}
+
+	p {
+		margin-top: 0;
+	}
+</style>
