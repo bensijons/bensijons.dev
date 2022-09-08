d553c944dff3bbbb84b433ce40100ad41eafa678
Benedikt Jónsson <bensijons@gmail.com>
2022-09-08 21:12:58 +0000

    remove snark

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,6 @@
 	<p>My name is Benedikt but you can call me Bensi</p>
 	<p>I work <a href="https://gangverk.is">@Gangverk</a></p>
 	<p>My handle is @bensijons on most places</p>
-	<p>For those wondering, the answer is yes, I created this website all by myself</p>
 </section>
 
 <style>
