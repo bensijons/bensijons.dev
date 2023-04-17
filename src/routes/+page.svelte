7b607745f571092c4d44251405672b4f1c00cc5a
Benedikt Jónsson <bensijons@gmail.com>
2023-04-17 22:40:26 +0000

    links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,13 @@
 	<h1 class="text-xl font-bold">Hi!</h1>
 	<p class="mt-4">
 		My name is Benedikt but you can call me Bensi <br />
-		I work <a class="underline" href="https://gangverk.is">@Gangverk</a>
+		I work at
+		<a class="underline hover:text-blue-800" href="https://gangverk.is">Gangverk</a>
 		<br />
-		My handle is @bensijons on most places
+		My handle is @bensijons on most places <br />
+		For example
+		<a class="underline hover:text-blue-800" href="https://github.com/bensijons">Github</a>
+		and
+		<a class="underline hover:text-blue-800" href="https://twitter.com/bensijons">Twitter</a>
 	</p>
 </section>
