b421ae86d617bd4fdcec3a7a4a1a16e94279ebd1
Benedikt Jónsson <bensijons@gmail.com>
2023-04-17 22:40:42 +0000

    margin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <section class="grid justify-center items-center">
 	<h1 class="text-xl font-bold">Hi!</h1>
-	<p class="mt-4">
+	<p class="mt-1">
 		My name is Benedikt but you can call me Bensi <br />
 		I work at
 		<a class="underline hover:text-blue-800" href="https://gangverk.is">Gangverk</a>
