f6e0fb0081a33fd64e5a4447ce7b311deb8633f4
Benedikt Jónsson <bensijons@gmail.com>
2023-11-20 22:45:37 +0000

    update text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,11 @@
 <section class="grid justify-center items-center">
 	<h1 class="text-xl font-bold">Hi!</h1>
-	<p class="mt-1">
+	<p class="mt-1 leading-7">
 		My name is Benedikt but you can call me Bensi <br />
 		I work at
 		<a class="underline hover:text-blue-800" href="https://gangverk.is">Gangverk</a>
 		<br />
-		My handle is @bensijons on most places <br />
-		For example
+		Find me on
 		<a class="underline hover:text-blue-800" href="https://github.com/bensijons">Github</a>
 		and
 		<a class="underline hover:text-blue-800" href="https://twitter.com/bensijons">Twitter</a>
